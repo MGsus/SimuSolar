33b67f47916b44e82aec1199cd0c385acb7474a2
Jesús Montoya <yiyismon.14@gmail.com>
2018-09-09 00:53:00 -0500

    unfinished mesh code

--- Assets/Scripts/MeshTriangle.cs
+++ Assets/Scripts/MeshTriangle.cs
@@ -1,16 +1,12 @@
 ﻿using UnityEngine;
 
 public class MeshTriangle : MonoBehaviour {
-
+    
     public Vector3 leftTopFront = new Vector3(-1, 1, 1);
-    public Vector3 rightTopFront = new Vector3(-1, 1, 1);
-    public Vector3 leftBottomFront = new Vector3(1, 1, 1);
-    public Vector3 rightBottomFront = new Vector3(-1, -1, 1);
-    public Vector3 leftTopBack = new Vector3(1, 1, 1);
-    public Vector3 rightTopBack = new Vector3(1, 1, 1);
-    public Vector3 leftBottomBack = new Vector3(1, 1, 1);
-    public Vector3 rightBottomBack = new Vector3(1, 1, 1);
-
+    public Vector3 rightTopFront = new Vector3(1, 1, 1);
+    public Vector3 rightTopBack = new Vector3(1, 1, -1);
+    public Vector3 leftTopBack = new Vector3(-1, 1, -1);
+    
     // Use this for initialization
     private void Start()
     {
@@ -19,23 +15,42 @@ public class MeshTriangle : MonoBehaviour {
         var vertices = new Vector3[]
         {
             // Front face
-            leftTopFront,
-            rightTopFront,
-            leftBottomFront,
-            rightBottomFront,
-
-            //Back face
-            leftTopBack,
-            rightTopBack,
-            leftBottomBack,
-            rightBottomBack
+            leftTopFront, // 0
+            rightTopFront, // 1
+            new Vector3(-1, -1, 1), //2
+            new Vector3(1, -1, 1), // 3
+            // Back face
+            rightTopBack, // 4
+            leftTopBack, // 5
+            new Vector3(1, -1, -1), // 6
+            new Vector3(-1, -1, -1), // 7
+            // Left face
+            leftTopBack, //8
+            leftTopFront, // 9
+            new Vector3(-1, -1, -1), // 10
+            new Vector3(-1, -1, 1), // 11
+            // Right face
+            rightTopFront, // 12
+            rightTopBack, // 13
+            new Vector3(), // 14
+            new Vector3(), // 15
+            // Top face
+            leftTopBack, // 16
+            rightTopBack, // 17
+            leftTopFront, // 18
+            rightTopFront, // 19
+            // Bottom face
+            new Vector3(-1, -1, 1), // 20
+            new Vector3(1, -1, 1), // 21
+            new Vector3(-1, -1, -1), // 22
+            new Vector3(1, -1, -1) // 23
         };
 
-        var triangle = new int[]
+        var triangles = new int[]
         {
             // Front face
-            0, 2, 3,
-            3, 1, 0,
+            0, 2, 3, // First triangle
+            3, 1, 0, // Second triangle
 
             // Back face
             4, 6, 7,
@@ -46,69 +61,57 @@ public class MeshTriangle : MonoBehaviour {
             11, 9, 8,
 
             // Right face
-            12,14,15,
-            15,13,12,
-            
+            12, 14, 15,
+            15, 13, 12,
+
             // Top face
-            16,18,19,
-            19,17,16,
-            
+            16, 18, 19,
+            19, 17, 16,
+
             // Bottom face
-            20,22,23,
-            23,21,20
+            20, 22, 23,
+            23, 21, 20
         };
 
         var uvs = new Vector2[]
         {
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0), 
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
-            new Vector2(0,1),
-            new Vector2(0,0),
-            new Vector2(1,1),
-            new Vector2(1,0),
-            
+            // Front face
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+            // Back Face
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+            // Left face
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+            // Right face
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+            // Top face
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 1),
+            new Vector2(1, 0),
+            // Bottom face
+            new Vector2(0, 1),
+            new Vector2(0, 0),
+            new Vector2(1, 1),
+            new Vector2(1, 0)
         };
         
         mesh.Clear();
         mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
     }
 	
     // Update is called once per frame
